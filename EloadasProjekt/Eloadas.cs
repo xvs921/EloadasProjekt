828e7fb1792a078db16da09a06e666c6bb04bcbf
VorakGergo <vorakgeri@gmail.com>
2020-02-21 11:00:59 +0100

    eloadas.cs kész

--- EloadasProjekt/Eloadas.cs
+++ EloadasProjekt/Eloadas.cs
@@ -8,5 +8,82 @@ namespace EloadasProjekt
 {
     public class Eloadas
     {
+        private bool[,] foglalasok;
+        int sorokSzama;
+        int helyekSzama;
+        int ferohelyek = 0;
+        int szabadHelyekSzama = 0;
+        int foglalt = 0;
+
+        public Eloadas(int sorokSzama,int helyekSzama)
+        {
+            if(sorokSzama<=0 || helyekSzama<=0)
+            {
+                throw new ArgumentException("Pozitív érték kell legyen");             
+            }
+            this.sorokSzama = sorokSzama;
+            this.helyekSzama = helyekSzama;
+            this.foglalasok = new bool[sorokSzama, helyekSzama];
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < helyekSzama; j++)
+                {
+                    foglalasok[sorokSzama, helyekSzama] = false;
+                    ferohelyek++;
+                }       
+            }
+        }
+        public bool Lefoglal()
+        {
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < helyekSzama; j++)
+                {
+                    if(foglalasok[i, j] == false)
+                    {
+                        foglalasok[i, j] = true;
+                        foglalt++;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public int SzabadHelyek
+        {
+            get
+            {
+                for (int i = 0; i < sorokSzama; i++)
+                {
+                    for (int j = 0; j < helyekSzama; j++)
+                    {
+                        if (foglalasok[i, j] == false)
+                        {
+                            szabadHelyekSzama++;
+                        }
+                    }
+                }
+                return szabadHelyekSzama;
+            }
+        }
+        public bool Teli()
+        {
+            if(ferohelyek==foglalt)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam>0 && helySzam>0)
+            {
+                return foglalasok[sorSzam - 1, helySzam - 1];
+            }
+            else
+            {
+                throw new ArgumentException("Érvénytelen paraméter");
+            }
+        }
     }
 }
---
+++ EloadasProjekt/test/EloadasTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EloadasProjekt.test
+{
+    [TestFixture]
+    class EloadasTest
+    {
+        Eloadas el;
+        [SetUp]
+        public void SetUp()
+        {
+            el = new Eloadas(5, 5);
+        }
+
+        [TestCase]
+        public void 
+
+    }
+}
